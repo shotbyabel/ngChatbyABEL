7e0ae0b9d3dea0d88d3fb42a0d38781fef1375e2
kimo5532 <shotbyabel@gmail.com>
2015-10-30 17:38:33 -0700

    first local init

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name ngChatbyAbelApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the ngChatbyAbelApp
+ */
+angular.module('ngChatbyAbelApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name ngChatbyAbelApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the ngChatbyAbelApp
+ */
+angular.module('ngChatbyAbelApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
